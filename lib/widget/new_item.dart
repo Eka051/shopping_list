49d7099fab2b7490a527d30cab8072d3b3e0292e
Dian Eka Raharjo <dianekaraharjo@gmail.com>
2025-02-02 22:21:37 +0700

    feat: enhance new item form with validation and category selection

--- lib/widget/new_item.dart
+++ lib/widget/new_item.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shopping_list/data/categories.dart';
+import 'package:shopping_list/data/dummy_items.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -13,9 +16,59 @@ class _NewItemState extends State<NewItem> {
       appBar: AppBar(
         title: const Text('Add New Item'),
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(12),
-        child: Text('Add a new item here'),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Form(
+          child: Column(
+            children: [
+              TextFormField(
+                maxLength: 50,
+                decoration: const InputDecoration(
+                  label: Text('Name'),
+                ),
+                validator: (value) {
+                  return 'Demoo';
+                },
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        label: Text('Quantity'),
+                      ),
+                      initialValue: '1',
+                      keyboardType: TextInputType.number,
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  Expanded(
+                    child: DropdownButtonFormField(items: [
+                      for (final category in categories.entries)
+                        DropdownMenuItem(
+                          value: category.value,
+                          child: Row(
+                            children: [
+                              Container(
+                                width: 16,
+                                height: 16,
+                                color: category.value.color,
+                              ),
+                              const SizedBox(width: 8),
+                              Text(category.value.title),
+                            ],
+                          ),
+                        ),
+                    ], onChanged: (context) {
+                      print('Category changed');
+                    }),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
